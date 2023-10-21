69c25b50a15262d498599c4ff8666e47732a911e
anikdas400 <anikchandra400@gmail.com>
2023-10-21 12:37:47 +0600

    general route setup complite

---
+++ src/Home/Home.jsx
@@ -0,0 +1,13 @@
+import Footer from "./Footer";
+
+
+const Home = () => {
+    return (
+        <div>
+            <h2>this is home section</h2>
+            <Footer></Footer>
+        </div>
+    );
+};
+
+export default Home;
---
+++ src/Root/Navber.jsx
@@ -0,0 +1,11 @@
+
+
+const Navber = () => {
+    return (
+        <div>
+            <h2>this is navber</h2>
+        </div>
+    );
+};
+
+export default Navber;
