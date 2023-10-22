e0d8b1fd6a44654549c6100b758128a43e7dfdd9
anikdas400 <anikchandra400@gmail.com>
2023-10-22 17:04:46 +0600

    Delete function layout complite

--- src/Product/CarCart.jsx
+++ src/Product/CarCart.jsx
@@ -1,7 +1,42 @@
+import Swal from "sweetalert2";
 
 
 const CarCart = ({car}) => {
     const {_id,brand,type,photo,price,description,rating} = car
+
+    const handleDelete = _id =>{
+     console.log(_id)
+     Swal.fire({
+        title: 'Are you sure?',
+        text: "You won't be able to revert this!",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Yes, delete it!'
+      }).then((result) => {
+        if (result.isConfirmed) {
+
+            fetch(`http://localhost:5000/cars/${_id}`,{
+                method:'DELETE'
+            })
+            .then(res=>res.json())
+            .then(data=>{
+                console.log(data)
+                if(data.deletedCount > 0){
+                    Swal.fire(
+                        'Deleted!',
+                        'Your car has been deleted.',
+                        'success'
+                      )
+                }
+            })
+
+
+          
+        }
+      })
+    }
     return (
         <div className="card bg-base-200 shadow-sm">
             <figure><img className="h-64 w-72" src={photo} alt="Shoes" /></figure>
@@ -15,9 +50,10 @@ const CarCart = ({car}) => {
                 <div className="card-actions justify-center">
                     <button className="btn bg-gradient-to-r from-purple-500 to-pink-500">Details</button>
                     <button className="btn bg-gradient-to-r from-violet-500 to-fuchsia-500">Update</button>
-                    <button className="btn bg-gradient-to-r from-cyan-500 to-blue-500">Delete</button>
-                    {/* <div className="badge "></div>
-                    <div className="badge badge-outline">Delete</div> */}
+                    <button 
+                    onClick={()=>handleDelete(_id)}
+                    className="btn bg-gradient-to-r from-cyan-500 to-blue-500">Delete</button>
+                    
                 </div>
             </div>
         </div>
