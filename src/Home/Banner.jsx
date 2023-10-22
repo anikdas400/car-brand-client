f878023f4a17e8f8e28d5e7a1b76511a3f105260
anikdas400 <anikchandra400@gmail.com>
2023-10-22 11:48:38 +0600

    add car section layout complite

--- src/Home/Banner.jsx
+++ src/Home/Banner.jsx
@@ -2,8 +2,8 @@
 
 const Banner = () => {
     return (
-        <div className="carousel w-full h-[500px] mx-auto ">
-            <div id="slide1" className="carousel-item relative w-full">
+        <div className="carousel w-full h-[32rem] mx-auto ">
+            <div id="slide1" className="carousel-item relative w-full ">
                 <img src="https://i.ibb.co/S7SksbR/banner-hy-time-v2.jpg" className="w-full"/>
                 <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
                     <a href="#slide4" className="btn btn-circle">❮</a>
--- src/Product/AddProduct.jsx
+++ src/Product/AddProduct.jsx
@@ -2,8 +2,77 @@
 
 const AddProduct = () => {
     return (
-        <div>
-            <h2>this is addProduct</h2>
+        <div className="bg-[#F4F3F0] P-8 rounded-lg">
+            <h1 className="text-5xl font-bold italic">Add New Car</h1>
+            <p className="py-6">Exploring the Cutting-Edge Automotive Landscape: Unveiling QuantumDrive™ Hybrid Electric Vehicles and Their Industry-Leading Peers from Renowned Brands for a Sustainable and Thrilling Future of Mobility.</p>
+            <form className="p-8">
+                {/* form row-1-brandName,type */}
+                <div className="flex gap-3 mb-3">
+                    <div className="form-control md:w-1/2">
+                        <label className="label">
+                            <span className="label-text font-semibold italic">Brand Name</span>
+                        </label>
+                        <label className="input-group">
+
+                            <input type="text" placeholder="Brand Name" name="brand" className="input input-bordered w-full" />
+                        </label>
+                    </div>
+                    <div className="form-control  md:w-1/2">
+                        <label className="label">
+                            <span className="label-text font-semibold italic">Car Types</span>
+                        </label>
+                        <label className="input-group">
+
+                            <input type="text" placeholder="Car Types" name="type" className="input input-bordered w-full" />
+                        </label>
+                    </div>
+                </div>
+                {/* form row-2-Photo URL,Price */}
+                <div className="flex gap-3 mb-3">
+                    <div className="form-control md:w-1/2">
+                        <label className="label">
+                            <span className="label-text font-semibold italic">Photo URL</span>
+                        </label>
+                        <label className="input-group">
+
+                            <input type="text" placeholder="Photo URL" name="photo" className="input input-bordered w-full" />
+                        </label>
+                    </div>
+                    <div className="form-control  md:w-1/2">
+                        <label className="label">
+                            <span className="label-text font-semibold italic">Price</span>
+                        </label>
+                        <label className="input-group">
+
+                            <input type="text" placeholder="Car Price" name="price" className="input input-bordered w-full" />
+                        </label>
+                    </div>
+                </div>
+                {/* form row-3-Description, */}
+                <div className="flex gap-3 mb-3">
+                    <div className="form-control md:w-1/2">
+                        <label className="label">
+                            <span className="label-text font-semibold italic">Description</span>
+                        </label>
+                        <label className="input-group">
+
+                            <input type="text" placeholder="Description" name="Description" className="input input-bordered w-full" />
+                        </label>
+                    </div>
+                    <div className="form-control  md:w-1/2">
+                        <label className="label">
+                            <span className="label-text font-semibold italic">Rating Us</span>
+                        </label>
+                        <label className="input-group">
+
+                            <input type="text" placeholder="Car Types" name="type" className="input input-bordered w-full" />
+                        </label>
+                    </div>
+                </div>
+               
+                <input type="submit" value="Add Car" className="btn btn-accent w-full italic" />
+
+            </form>
         </div>
     );
 };
